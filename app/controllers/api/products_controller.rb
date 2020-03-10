a71d5337645975375e698e6b296904d001d391c9
ryan <ryanwaszak@gmail.com>
2020-03-09 20:04:14 -0500

    add is_discounted, tax, and total model methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -28,6 +28,7 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
+    @product.save
     render "show.json.jb"
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+      "True"
+    else
+      "False"
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
