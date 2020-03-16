23e01a0d240889d2ed041debb44f5c887938772d
ryan <ryanwaszak@gmail.com>
2020-03-16 18:44:47 -0500

    add product_id to Image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_232612) do
+ActiveRecord::Schema.define(version: 2020_03_16_234028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_232612) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "product_id"
   end
 
   create_table "products", force: :cascade do |t|
