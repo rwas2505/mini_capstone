be0c8c3d545a463c818268139fa9543aff68adff
ryan <ryanwaszak@gmail.com>
2020-03-25 21:38:32 -0500

    add edit and destroy routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,5 +12,37 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render 'show.html.erb'
   end
-  
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+    )
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+    )
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to index.html
+  end
 end
